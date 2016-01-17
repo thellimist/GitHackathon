ecb66658c9c002cfe8d7702b79a9e6cf23c6200a
thellimist <mufuyil@gmail.com>
2016-01-17 14:02:30 -0800

    LandingVC tweaked

--- githackathon/githackathon/LandingVC.swift
+++ githackathon/githackathon/LandingVC.swift
@@ -14,8 +14,7 @@ class LandingVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.purpleColor()
         
-        let scaledImage = Utility.imageResize(UIImage(named: "BackgroundLanding")!, sizeChange: CGSize(width: ez.screenWidth, height: ez.screenHeight))
-        
+        let scaledImage = Utility.imageResize(UIImage(named: "SplashScreen")!, sizeChange: CGSize(width: ez.screenWidth, height: ez.screenHeight))
         self.view.backgroundColor = UIColor(patternImage: scaledImage)
     }
     
@@ -24,6 +23,35 @@ class LandingVC: UIViewController {
         
         sleep(1)
         
+        let scaledImage = Utility.imageResize(UIImage(named: "LandingBackground")!, sizeChange: CGSize(width: ez.screenWidth, height: ez.screenHeight))
+        self.view.backgroundColor = UIColor(patternImage: scaledImage)
+        
+        let xButton = UIButton(x: 40, y: 440, w: 300, h: 50)
+        xButton.backgroundColor = UIColor.clearColor()
+        xButton.setTitle("FOLLOW ALONG LIVE GAME", forState: UIControlState.Normal)
+        xButton.titleLabel?.font = UIFont(name: Utility.ThemeFontName, size: 16)
+        xButton.layer.borderWidth = 1
+        xButton.layer.borderColor = UIColor.whiteColor().CGColor
+        xButton.alpha = 0.2
+//        xButton.addTarget(self, action: "buttonAction", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(xButton)
+        
+        let yButton = UIButton(x: 40, y: 510, w: 300, h: 50)
+        yButton.backgroundColor = UIColor.clearColor()
+        yButton.setTitle("BASIC FOOTBALL KNOWLEDGE", forState: UIControlState.Normal)
+        yButton.titleLabel?.font = UIFont(name: Utility.ThemeFontName, size: 16)
+        yButton.layer.borderWidth = 1
+        yButton.layer.borderColor = UIColor.whiteColor().CGColor
+        yButton.addTarget(self, action: "buttonAction", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(yButton)
+        
+    }
+    
+    
+    
+    func buttonAction() {
         self.presentViewController(VCOrganizer.MainVCHolder, animated: true, completion: nil)
     }
+
 }
+
---
+++ githackathon/githackathon/PersonalityView.swift
@@ -0,0 +1,9 @@
+//
+//  PersonalityView.swift
+//  githackathon
+//
+//  Created by Furkan Yilmaz on 17/01/16.
+//  Copyright © 2016 Furkan Yilmaz. All rights reserved.
+//
+
+import Foundation
