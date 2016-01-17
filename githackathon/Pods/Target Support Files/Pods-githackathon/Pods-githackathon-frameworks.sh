bb22ff1f869d887bd425e87dc964a9f210bb006e
thellimist <mufuyil@gmail.com>
2016-01-16 20:10:51 -0800

    Added Template for Network Router

--- githackathon/Pods/Target Support Files/Pods-githackathon/Pods-githackathon-frameworks.sh
+++ githackathon/Pods/Target Support Files/Pods-githackathon/Pods-githackathon-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-githackathon/Alamofire.framework"
   install_framework "Pods-githackathon/EZSwiftExtensions.framework"
   install_framework "Pods-githackathon/EZSwipeController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-githackathon/Alamofire.framework"
   install_framework "Pods-githackathon/EZSwiftExtensions.framework"
   install_framework "Pods-githackathon/EZSwipeController.framework"
 fi
--- githackathon/githackathon/Network.swift
+++ githackathon/githackathon/Network.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2016 Furkan Yilmaz. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+import UIKit
+
+struct IBMWatson {
+    
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.instagram.com"
+        static let clientID = "31aeb778aa24439fafcf32bf22643b16"
+        static let redirectURI = "http://www.example.com/"
+        static let clientSecret = "53324118a75c433b92bb135a9e1b4b8f"
+        
+        case Mood(String)
+        
+        var URLRequest: NSMutableURLRequest {
+            let result: (path: String, parameters: [String: AnyObject]?) = {
+                switch self {
+                case .Mood(let accessToken):
+                    let params = ["access_token": accessToken]
+                    let pathString = "/v1/media/popular"
+                    return (pathString, params)
+                }
+            }()
+            
+            let BaeseURL = NSURL(string: Router.baseURLString)!
+            let URLRequest = NSURLRequest(URL: BaeseURL.URLByAppendingPathComponent(result.path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            return encoding.encode(URLRequest, parameters: result.parameters).0
+        }
+        
+    }
+}
